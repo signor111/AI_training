e903f28caa902e19c3e4c16b2fe9801ab18cf718
signor <772625328@qq.com>
2024-10-17 09:40:27 +0800

    Git LFS commit

---
+++ generate_sentence.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2d89ee7aee3694c3372b27c4b5debc81666c448420b8b7dda887cfac5de24799
+size 1771
---
+++ train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0872be2f7f35631304dcfdbde7c3426d299c5ac1a338b18a4121d90dbdaf2844
+size 2176
